2b954f2bba94c73757292146ee9bf7364eb49c33
sapinux <sapinux@gmail.com>
2025-05-26 16:29:38 -0300

    ajuste para evitar erro do numero da sala

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const wss = new WebSocket.Server({ port: 3000 });
 
 const rooms = {}; // Ex: { sala1: Set([...sockets]), sala2: Set([...]) }
 const clientRooms = new Map(); // Associa cada cliente à sua sala
+//const sala = new Map(); //Associa o cada sala a quantidade de clientes
+var count_sala = 0
+//var sala_aberta = true;   //controlar a disponibilidade da sala atual
 
 wss.on('connection', (ws) => {
     // código que deve ser executado logo após o jogador se conectar
@@ -23,39 +26,44 @@ wss.on('connection', (ws) => {
             case "create_player_request":   //criar sala aqui
                             
                 //verifica se existe sala
-                if (Object.keys(rooms).length == 0)  rooms[1] = new Set();  //caso contrario sera criada uma com o indice "1"
+                if (Object.keys(rooms).length == 0)  {
+                    count_sala ++
+                    rooms[count_sala] = new Set();  //caso contrario sera criada uma sala com o indice "1"
+                }
                 
                 // Verifica se a sala room já existe no objeto rooms. 
                                           
-                if (rooms[Object.keys(rooms).length].size < 3) {     //se a sala estiver abaixo do limite
+                if (rooms[count_sala].size < 3 ) {//&& sala_aberta) {     //se a sala estiver abaixo do limite
 
                     // Adiciona o WebSocket ws (a conexão do cliente) ao conjunto de clientes da sala. 
                     // Isso significa que o cliente agora "entrou" na sala.
-                    rooms[Object.keys(rooms).length].add(ws);
+                    rooms[count_sala].add(ws);
                                        
                     // clientRooms é um Map que associa cada cliente à sala que ele entrou.
                     // Isso é útil para Saber em que sala o cliente está.
                     // Enviar mensagens apenas para clientes da mesma sala.
                     // Remover o cliente da sala certa quando ele desconectar.
-                    clientRooms.set(ws, Object.keys(rooms).length);  // Mapeia a conexão ws com a sala room.
+                    clientRooms.set(ws, count_sala);  // Mapeia a conexão ws com a sala room.
                 } else {    
-                    rooms[Object.keys(rooms).length + 1] = new Set(); 
-                    rooms[Object.keys(rooms).length].add(ws);
-                    clientRooms.set(ws, Object.keys(rooms).length);  // Mapeia a conexão ws para a sala room.
+                    count_sala ++;  
+                    rooms[count_sala] = new Set(); 
+                    rooms[count_sala].add(ws);
+                    clientRooms.set(ws, count_sala);  // Mapeia a conexão ws para a sala room.
+                    //sala_aberta = true;     //abre a sala atual
                 }
 
                 console.log("Total de salas: " + Object.keys(rooms).length);                    //depuração
-                console.log("Total de jogadores: " + rooms[Object.keys(rooms).length].size);    //depuração
+                console.log("Total de jogadores: " + rooms[count_sala].size);    //depuração
                
-                ws.send(JSON.stringify({ event_name: 'Você foi criado!', id: rooms[Object.keys(rooms).length].size, sala: Object.keys(rooms).length }));
-                if (rooms[Object.keys(rooms).length].size > 1) {
+                ws.send(JSON.stringify({ event_name: 'Você foi criado!', id: rooms[count_sala].size, sala: count_sala }));
+                if (rooms[count_sala].size > 1) {
                     const room = clientRooms.get(ws);
 
                     // Envia para todos da sala (exceto o remetente)
                     // Percorre todos os clientes CONECTADOS à sala especificada.
                     rooms[room].forEach(client => {
                         if (client !== ws && client.readyState === WebSocket.OPEN) {
-                            client.send(JSON.stringify({ event_name: 'Jogador na sala!', sala: room, jogador: rooms[Object.keys(rooms).length].size}));
+                            client.send(JSON.stringify({ event_name: 'Jogador na sala!', sala: room, jogador: rooms[room].size}));
                         }
                     })
                 }
@@ -87,6 +95,7 @@ wss.on('connection', (ws) => {
 
                 break;
             case "iniciar_partida":
+                //sala_aberta = false;    //fecha a sala atual
                 room = clientRooms.get(ws);
                         // Envia para todos da sala (exceto o remetente)
                         // Percorre todos os clientes CONECTADOS à sala especificada.
@@ -199,6 +208,8 @@ wss.on('connection', (ws) => {
             if (rooms[room].size === 0) delete rooms[room]; // Se não existe cliente na sala, delete-a
             clientRooms.delete(ws);
             
+            if (Object.keys(rooms).length == 0) count_sala = 0; //reinicia o contador
+
             console.log("Total de salas: " + Object.keys(rooms).length);    //depuração
             
         }
